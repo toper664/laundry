f8f3b5a11f079eddaac936a5349dcbc0e889f46c
Coding <102744661+toper664@users.noreply.github.com>
2026-05-06 18:53:47 +0700

    axios removal due to security vuln

--- src/modules/simulator/simulator.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { manualSim, autoSim } from './simulator.controller.ts';
-
-const router = Router();
-
-router.post('/control', manualSim);
-router.post('/auto', autoSim);
-
-export default router;
